2a325ba202613ec34a4a0da15fab180c6639bc8e
James Campbell <james@supmenow.com>
2015-09-23 06:29:56 +0100

    Initial Commit

---
+++ QuartzMobile/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  QuartzMobile
+//
+//  Created by James Campbell on 23/09/2015.
+//  Copyright © 2015 James Campbell. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ QuartzMobile/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  QuartzMobile
+//
+//  Created by James Campbell on 23/09/2015.
+//  Copyright © 2015 James Campbell. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
