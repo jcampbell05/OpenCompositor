f296bd300c288a930b59f05df7c311e62466d0ee
James Campbell <james@supmenow.com>
2015-09-23 07:21:17 +0100

    Adds in image provider.

---
+++ QuartzMobile/QuartzCompositor/QCImageBufferProvider.h
@@ -0,0 +1,13 @@
+//
+//  QCImageBufferProvider.h
+//  QuartzMobile
+//
+//  Created by James Campbell on 23/09/2015.
+//  Copyright © 2015 James Campbell. All rights reserved.
+//
+
+#import "QCImageProvider.h"
+
+@interface QCImageBufferProvider : QCImageProvider
+
+@end
---
+++ QuartzMobile/QuartzCompositor/QCImageBufferProvider.m
@@ -0,0 +1,13 @@
+//
+//  QCImageBufferProvider.m
+//  QuartzMobile
+//
+//  Created by James Campbell on 23/09/2015.
+//  Copyright © 2015 James Campbell. All rights reserved.
+//
+
+#import "QCImageBufferProvider.h"
+
+@implementation QCImageBufferProvider
+
+@end
